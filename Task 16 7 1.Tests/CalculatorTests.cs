8d449dab4f2b6710f6c03dd56d2fec28c5c93522
Sergey Bednyakov <sbednyakov@gmail.com>
2022-07-27 14:33:21 +0300

    Add project files.

---
+++ Task 16 7 1.Tests/CalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_16_7_1;
+
+namespace Task_16_7_1.Tests
+{
+    public class CalculatorTests
+    {
+        Calculator _calculator;
+        public CalculatorTests()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Test]
+        public void AdditionMustRecurnCorrectValue()
+        {
+            Assert.AreEqual(4, _calculator.Addition(1, 3));
+        }
+
+        [Test]
+        public void SubstractionMustRecurnCorrectValue()
+        {
+            Assert.AreEqual(3, _calculator.Subtraction(8, 5));
+        }
+
+        [Test]
+        public void MultiplicationMustRecurnCorrectValue()
+        {
+            Assert.AreEqual(40, _calculator.Multiplication(8, 5));
+        }
+
+        [Test]
+        public void DivisionMustRecurnCorrectValue()
+        {
+            Assert.AreEqual(2, _calculator.Division(14, 5));
+        }
+    }
+}
